f6a990ba0ed032c7551034b09e345cc04f95b1f9
wiese4 <michael.stahl@stud.uni-regensburg.de>
2016-10-24 16:04:09 +0200

    Initial commit. Added Code Structure.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/* global module*/
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        concat: {
+            options: {
+                separator: ";"
+            },
+            dist: {
+                src: [""],
+                dest: "dist/<%= pkg.name %>.js"
+            },
+            examples: {
+                src: [""],
+                dest: "examples/<%= pkg.name %>.js"
+            }
+        },
+        uglify: {
+            options: {
+                banner: "/*! <%= pkg.name %> <%= grunt.template.today('dd-mm-yyyy') %> */\n"
+            },
+            dist: {
+                files: {
+                    "dist/<%= pkg.name %>.min.js": ["<%= concat.dist.dest %>"]
+                }
+            },
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+
+    grunt.registerTask("default", ["concat", "uglify"]);
+};
